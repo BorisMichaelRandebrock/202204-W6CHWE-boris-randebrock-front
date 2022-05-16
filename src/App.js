628b8a9eef015f6a7677f37235f15e76a3599374
Boris <boris@randebrock.com>
2022-05-16 19:18:23 +0200

    added som text to robot page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="container"></div>;
+  return <div className="container">Robots... coming soon ... (or never)</div>;
 }
 
 export default App;
