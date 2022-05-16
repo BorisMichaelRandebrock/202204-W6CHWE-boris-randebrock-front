b7611d67b5b23927504113907e618be2f0758b99
Boris <boris@randebrock.com>
2022-05-16 19:14:17 +0200

    nothing todo.... initiated things for painting

---
+++ components/Robot.js
@@ -0,0 +1,19 @@
+const Robot = () => {
+  return (
+    <li>
+      <h2>RobotName</h2>
+      <img src="url" alt="robot" />
+      <p>
+        <sp>speed</sp>
+      </p>
+      <p>
+        <sp>resistance</sp>
+      </p>
+      <p>
+        <sp>creationDate</sp>
+      </p>
+    </li>
+  );
+};
+
+export default Robot;
