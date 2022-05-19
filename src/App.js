3b296f595110ecea58573f45e050b1ee8b977c0a
Boris <boris@randebrock.com>
2022-05-19 15:38:25 +0200

    added minimum styling

--- src/App.js
+++ src/App.js
@@ -7,20 +7,12 @@ import { loadRobotsThunk } from "./redux/thunks/robotsThunks";
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(loadRobotsThunk);
+    dispatch(loadRobotsThunk());
   }, [dispatch]);
   return (
     <div className="container">
       <h1>Robots (or not.... ?)</h1>
-      <p>🤖 👾 🎃 👹 👽 🤖</p>
-      <p className="titles">toby poo boo foo zoby sonny</p>
-      <p>🤡 👻 ☠️ 🙀 😹 🤡</p>
-      <p className="titles">wiru roby niu tab byz som</p>
-      <p>🥶 💩 🫣 🫥 👁 🤖</p>
-      <p className="titles">fili poo cuby tara bzzz r2d2</p>
-      <div className="container">
-        <RobotList />
-      </div>
+      <RobotList />
     </div>
   );
 }
